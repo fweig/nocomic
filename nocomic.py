c183854bfc6bcf5a469743507b6dae99c7a873b8
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-08-16 21:49:38 +0200

    Send images base64 encoded.

--- nocomic.py
+++ nocomic.py
@@ -6,10 +6,27 @@ from http.server import (
         HTTPStatus,
 )
 
+from base64 import (
+        b64encode,
+)
+
 
 IP_ADDR = 'localhost'
 PORT = 8080
 SERVER_ADDR = (IP_ADDR, PORT)
+
+TEST_IMAGE = '/home/felix/Pictures/large.png'
+
+HTML_START = """
+<!DOCTYPE html>
+<html>
+  <body>
+"""
+
+HTML_END= """
+  </body>
+</html>
+"""
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -22,9 +39,23 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        message = "Hello, world!"
+        msgtemplate = '<img src="data:image/png;base64,{}">'
 
-        self.wfile.write(bytes(message, 'utf8'))
+        # TODO serve images over sepereate http request, this save us from base64 encoding
+        with open(TEST_IMAGE, 'rb') as imgfile:
+            b64img = b64encode(imgfile.read())
+
+        self.sendimg(b64img)
+
+    def sendimg(self, b64img):
+        self.wfile.write(bytes(HTML_START, 'utf8'))
+        self.wfile.write(bytes('<img src="data:image/png;base64,', 'utf8'))
+        self.wfile.write(b64img)
+        self.sendstr('">')
+        self.sendstr(HTML_END)
+
+    def sendstr(self, txt):
+        self.wfile.write(bytes(txt, 'utf8'))
 
 
 
